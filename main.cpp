d9c988bbe2dc180f201937361ad073974f000131
F1renze <f1renze.142857@gmail.com>
2021-12-09 14:23:01 +0800

    lg: p1616（采药，完全背包）

--- main.cpp
+++ main.cpp
@@ -1,77 +1,28 @@
 //#include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
-int mx[505][505];
-int tmp[505][505];
-
-void rotate_mx(int x, int y, int n) {
-    if (n == 1)
-        return;
-
-    for (int i = x; i < x + n; i++)
-        for (int j = y; j < y + n; j++)
-            tmp[i][j] = mx[i][j];
-
-    int x1 = x + n-1, y1 = y;
-    for (int i = x; i < x + n; i++) {
-        for (int j = y; j < y + n; j++) {
-            mx[i][j] = tmp[x1][y1];
-            x1--;
-        }
-        x1 = x + n-1;
-        y1++;
-    }
-}
-
-void anticlockwise_rotate_mx(int x, int y, int n) {
-    if (n == 1)
-        return;
-
-    for (int i = x; i < x + n; i++)
-        for (int j = y; j < y + n; j++)
-            tmp[i][j] = mx[i][j];
-
-    int x1 = x, y1 = y + n-1;
-    for (int i = x; i < x + n; i++) {
-        for (int j = y; j < y + n; j++) {
-            mx[i][j] = tmp[x1][y1];
-            x1++;
-        }
-        y1--;
-        x1 = x;
-    }
-}
-
-void print_mx(int x, int y, int n) {
-    for (int i = x; i < x + n; i++) {
-        for (int j = y; j < y + n; j++)
-            cout << mx[i][j] << " ";
-        cout << endl;
-    }
-    cout << endl;
-}
 
 //bits/stdc++.h
 int main() {
     ios::sync_with_stdio(false);
-    int n, m;
-    cin >> n >> m;
+    int t, m;
+    cin >> t >> m;
 
-    int t = 1;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            mx[i][j] = t++;
+    int w[10001], c[10001];
+    for (int i = 1; i <= m; i++)
+        cin >> w[i] >> c[i];
 
-    int x, y, r, z;
-    for (int i = 0; i < m; i++) {
-        cin >> x >> y >> r >> z;
-        if (!z)
-            rotate_mx(x - r - 1, y - r - 1, 2 * r + 1);
-        else
-            anticlockwise_rotate_mx(x - r - 1, y - r - 1, 2 * r + 1);
-    }
-    print_mx(0, 0, n);
+    vector<long long> dp(t+1, 0);
+
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= t; j++)
+            if (j >= w[i])
+                dp[j] = max(dp[j], dp[j-w[i]] + c[i]);
+
+
+    cout << dp[t];
+
     return 0;
 }
