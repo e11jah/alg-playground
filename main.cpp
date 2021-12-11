ea3026b736967e6ab6c6c79c8d0816da21c01c80
F1renze <f1renze.142857@gmail.com>
2021-12-11 14:47:17 +0800

    fixup! lg: p1223 排队接水 排序贪心

--- main.cpp
+++ main.cpp
@@ -5,35 +5,28 @@
 using namespace std;
 
 int main() {
+    // 使用 pair 记录 idx
     using PII = pair<int, int>;
     int n, t;
-    cin >> n >> t;
-    // mi, vi
-    vector<PII> wv(n);
-
-    for (int i = 0; i < n; i++)
-        cin >> wv[i].first >> wv[i].second;
-
-    sort(wv.begin(), wv.end(), [&](const PII l, const PII r) -> bool {
-        // tips 分数比较大小
-        return  l.second * r.first > r.second * l.first;
-    });
-
-    // 6 5 4 3
-    // 40 - 20 + 20  s = 60 + 100 + 80 = 240
-    double s = 0;
-    for (PII p : wv) {
-        if (!t)
-            break;
-        if (t >= p.first) {
-            t -= p.first;
-            s += 1.0 * p.second;
-        } else {
-            s += 1.0 * t / p.first * p.second;
-            t -= t;
-        }
+    long long s = 0;
+    cin >> n;
+    vector<PII> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i].first;
+        arr[i].second = i+1;
     }
-    cout.precision(2);
-    cout << fixed << s;
+
+    stable_sort(arr.begin(), arr.end(), [](PII l, PII r) -> bool {
+        return l.first < r.first;
+    });
+    t = n-1;
+    for (PII e : arr) {
+        s += t * e.first;
+        t--;
+        cout << e.second << " ";
+    }
+    // cout.precision(2);
+    // cout << endl << fixed << (double) s / n;
+    printf("\n%.2lf",  (double) s / n);
     return 0;
 }
