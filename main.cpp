009f7a48141876c6a404d34b1ab19d437bd065c4
F1renze <f1renze.142857@gmail.com>
2021-12-14 20:11:25 +0800

    lg: p1106 删数 str 贪心

--- main.cpp
+++ main.cpp
@@ -2,31 +2,35 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-using long long = ll;
+typedef long long ll;
 int main() {
-    int n, m;
-    long long ans = 0;
-    cin >> n >> m;
+    string s;
+    int k;
+    cin >> s >> k;
 
-    vector<int> v(n);
-    cin >> v[0];
-    int preSum = v[0];
-    for (int i = 1; i < n; i++) {
-        cin >> v[i];
-        preSum += v[i];
-        if (preSum > m) {
-            preSum -= m;
-            ans += preSum;
-            preSum = v[i] > preSum ? v[i]-preSum : 0;
-        } else
-            preSum = v[i];
+    for (int j = k; j > 0; j--) {
+        for (int i = 0; i < s.size()-1; i++) {
+            // 删掉高位比地位大的数字
+            if ((s[i] - '0') > (s[i+1]-'0')) {
+                s.erase(s.begin() + i);
+                k--;
+                break;
+            }
+        }
     }
-    if (ans < 1)
-        ans = 1;
-    cout << ans;
+    // 若高位都比低位小，删除尾部
+    if (k) {
+        s = s.substr(0, s.size()-k);
+    }
+    // 去除前导0
+    size_t found = s.find_first_not_of('0');
+    if (found != string::npos)
+        s = s.substr(found);
+    cout << s;
 
     return 0;
 }
