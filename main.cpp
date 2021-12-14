a52956273f17cf479bcb654dec7dbcf00abe578c
F1renze <f1renze.142857@gmail.com>
2021-12-14 21:33:04 +0800

    lg: p4995 跳跳 贪心

--- main.cpp
+++ main.cpp
@@ -7,30 +7,55 @@
 using namespace std;
 
 typedef long long ll;
-int main() {
-    string s;
-    int k;
-    cin >> s >> k;
 
-    for (int j = k; j > 0; j--) {
-        for (int i = 0; i < s.size()-1; i++) {
-            // 删掉高位比地位大的数字
-            if ((s[i] - '0') > (s[i+1]-'0')) {
-                s.erase(s.begin() + i);
-                k--;
-                break;
-            }
+void v1() {
+    int n, cur, m, mi, t;
+    ll ans = 0;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    sort(v.begin(), v.end(), [](int l, int r) {
+        return l < r;
+    });
+    cur = v[n-1];
+    ans += cur * cur;
+    v.pop_back();
+    for (int j = 1; j < n; j++) {
+        m = 0;
+        for (int i = 0; i < v.size(); i++) {
+            t = abs(cur - v[i]);
+            if (t > m)
+                m = t, mi = i;
         }
+        ans += m * m;
+        cur = v[mi];
+        v.erase(v.begin()+mi);
     }
-    // 若高位都比低位小，删除尾部
-    if (k) {
-        s = s.substr(0, s.size()-k);
+    cout << ans;
+}
+int main() {
+    int n;
+    ll ans = 0;
+    cin >> n;
+    vector<int> arr(301, 0);
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+    sort(arr.begin()+1, arr.begin()+n+1);
+
+    int l = 0, r = n, k=1, t;
+    while (l < r) {
+        t = arr[r] - arr[l];
+        ans += t * t;
+        // 奇数次 左跳右, 下次反之
+        if (k&1)
+            l++;
+        // 偶数次 右跳左
+        else
+            r--;
+        k++;
     }
-    // 去除前导0
-    size_t found = s.find_first_not_of('0');
-    if (found != string::npos)
-        s = s.substr(found);
-    cout << s;
+    cout << ans;
 
     return 0;
 }
