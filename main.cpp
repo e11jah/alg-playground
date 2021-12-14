9ca11ce7c37c98c7e40663df10215fc66f45c60f
F1renze <f1renze.142857@gmail.com>
2021-12-14 18:25:17 +0800

    lg: p3397 地毯 前缀和

--- main.cpp
+++ main.cpp
@@ -6,20 +6,24 @@
 using namespace std;
 
 int main() {
-    int n, m = -(1<<30), s = 0;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
+    int mx[1001][1001];
+    memset(mx, 0, sizeof mx);
+    int x1, y1, x2, y2;
+    for (int i =0 ; i< m; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
 
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-        if (s > 0)
-            s += v[i];
-        else
-            s = v[i];
-
-        m = m > s ? m : s;
+        for (int x = x1 - 1; x < x2; x++)
+            for (int y = y1 - 1; y < y2; y++)
+                mx[x][y]++;
     }
 
-    cout << m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << mx[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
