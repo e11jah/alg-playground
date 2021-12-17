c9953baa28a9257368a26e779af7f747700cbf91
F1renze <f1renze.142857@gmail.com>
2021-12-18 00:29:06 +0800

    lg: p2004 领地选择 2d 前缀和

--- main.cpp
+++ main.cpp
@@ -11,34 +11,32 @@ typedef long long ll;
 
 
 int main() {
-    int n, m, a, b, c;
-    cin >> n >> m;
+    int mx[1001][1001], n, m, c;
+    vector<vector<ll>> sum(1001, vector<ll>(1001, 0ll));
+    cin >> n >> m >> c;
+    ll val = 0, t;
+    memset(mx, 0, sizeof mx);
+    /**
+     * sum i,j =  sum i-1,j + sum i,j-1 - sum i-1,j-1 + a i,j
+     * sum x2,y2 - sum x1-1,y2 - sum x2,y1-1 + sum x1-1,y1-1
+     */
+    int x1, y1, x, y;
+    for (int i = 1; i <= n ; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> mx[i][j];
+            sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + mx[i][j];
+            if (i >= c && j >= c) {
+                // x1, x2 已经为 左上角 -1
+                x1 = i-c, y1 = j-c;
+                t = sum[i][j] - sum[x1][j] - sum[i][y1] + sum[x1][y1];
 
-    vector<int> d(n);
-
-    int pre, p;
-    for (int i = 0; i < m; i++) {
-        cin >> p;
-        // i > 0
-        if (i) {
-            // 记录访问次数差分
-            if (p > pre)
-                d[pre]++, d[p]--;
-            else
-                d[p]++, d[pre]--;
+                if (t > val)
+                    x = x1+1, y = y1+1;
+                val = val > t ? val : t;
+            }
         }
-        pre = p;
     }
-    ll times = 0, ans = 0, t;
-    for (int i = 1; i < n; i++) {
-        cin >> a >> b >> c;
-        // times[i] 表 从 i 到 i + 1 的访问次数
-        // times[i] = times[i-1] + d[i];
-        times += d[i];
-        t = times * b + c;
-        ans += t > a * times ? a * times : t;
-    }
-    cout << ans;
+    cout << x << " " << y;
 
     return 0;
 }
