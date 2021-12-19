87688ee04322742a1778be6d9b4c28e8eeef194b
F1renze <f1renze.142857@gmail.com>
2021-12-19 19:52:03 +0800

    lc: 5958. 股票平滑下跌阶段的数目 滑动计数

--- lc.cc
+++ lc.cc
@@ -5,27 +5,34 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> l;
-        for (int i = 0; i < n; i++) {
-            auto it = lower_bound(l.begin(), l.end(), nums[i]);
-            // 若找不到比 i 大的元素，放入序列
-            if (it == l.end())
-                l.push_back(nums[i]);
-            else
-                // 否则修改第一个比 i 大的元素为 i （后续元素与 i 比较，同时保留前面的长度）
-                *it = nums[i];
+    // 等差求和
+    long long sum(long long x) {
+        return (1 + x) * x / 2;
+    }
+    long long getDescentPeriods(vector<int>& prices) {
+        int n = prices.size(), cur = 0;
+        long long ans = n;
+        int l = 1;
 
+        for (int i = 1; i < n; i++) {
+            if (prices[i] + 1== prices[i-1]) {
+                l++;
+            } else {
+                ans += sum(l-1);
+                l = 1;
+            }
         }
-        return l.size();
+        if (l > 1)
+            ans += sum(l-1);
+
+        return ans;
     }
 };
 
 int main() {
     vector<int> tt = {10,9,2,5,3,7,101,18};
     auto s = new Solution;
-    cout << s->lengthOfLIS(tt);
+//    cout << s->lengthOfLIS(tt);
 }
 
 
