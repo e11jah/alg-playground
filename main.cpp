1becffe6e750aa348c971172c5bb0e82e0ec371d
F1renze <f1renze.142857@gmail.com>
2021-12-19 19:43:03 +0800

    lg: p4447 纪念品分组 队列+贪心

--- main.cpp
+++ main.cpp
@@ -11,34 +11,24 @@ typedef long long ll;
 
 
 int main() {
-    int n, cnt = 0, pre, q;
-    cin >> n;
-    // 数值小的在队头, len 记录队伍长度
-    vector<int> v(n), end(n+1), len(n+1);
+    int n, w, cnt = 0, pre, q;
+    cin >> w >> n;
+    vector<int> v(n), val(n+1), len(n+1);
     for (int i = 0; i < n; i++)
         cin >> v[i];
 
-    // 排序，后面的元素都大于当前所有队列的队尾
-    sort(v.begin(), v.end());
-    // 题意：使得人数最少的组人数最多，即所有队伍（连续序列）的人数都尽可能接近
-    for (int i = 0; i < n; i++) {
-        // pre 最短队伍长度; q 队伍 idx
-        pre = n, q = 0;
-        for (int j = 1; j <= cnt; j++)
-            // 找到当前元素能加入的长度最短的队伍
-            if (end[j] + 1 == v[i] && len[j] < pre)
-                q = j, pre = len[j];
+    sort(v.begin(), v.end(), greater());
+    for (int e : v) {
+        q = 0, pre = w;
+        for (int i = cnt; i > 0; i--)
+            if (len[i] < 2 && val[i] + e <= w && val[i] < pre)
+                q = i, pre = val[i];
 
         if (!q)
-            end[++cnt] = v[i], len[cnt]++;
-        else
-            end[q] = v[i], len[q]++;
+            q = ++cnt;
 
+        val[q]+=e, len[q]++;
     }
-    q = n;
-    for (int i = 1; i <= cnt; i++)
-        q = len[i] < q ? len[i] : q;
-
-    cout << q;
+    cout << cnt;
     return 0;
 }
