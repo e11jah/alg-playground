b02920028388f34bbd7b5405890df8c9c96c5105
F1renze <f1renze.142857@gmail.com>
2021-12-19 23:53:26 +0800

    lg: p2678 跳石头 二分

--- main.cpp
+++ main.cpp
@@ -10,61 +10,41 @@ using PII = pair<int, int>;
 typedef long long ll;
 
 
-void dp() {
-    int n;
-    cin >> n;
-    vector<int> p1(n + 1), p2(n + 1);
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1));
-    for (int i = 1; i <= n; i++)
-        cin >> p1[i];
-    for (int i = 1; i <= n; i++)
-        cin >> p2[i];
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-            if (p1[i] == p2[i])
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
-        }
-    }
-    cout << dp[n][n];
-}
-
-/**
- * 转为 lis 问题
- */
-void lis() {
-    int n;
-    cin >> n;
-    vector<int> p1(n), p2(n), idx(n + 1), cl;
-    for (int i = 0; i < n; i++) {
-        cin >> p1[i];
-        idx[p1[i]] = i + 1;
-    }
-    /** 由于 p2 单调递增，公共元素列中单调递增的即为最长公共子序列
-     */
-    for (int i = 0; i < n; i++) {
-        cin >> p2[i];
-        // 用 0 区别, 长度 n+1
-        if (idx[p2[i]] > 0)
-            // p1 乱序，cl 中存放公共元素
-            cl.push_back(idx[p2[i]]);
-    }
-
-    vector<int> lis;
-    for (int & e : cl) {
-        auto it = lower_bound(lis.begin(), lis.end(), e);
-        // 公共子序列中找不到即前面都比 e 小
-        if (it == lis.end())
-            lis.push_back(e);
+bool check(vector<int> loc, int mid, int m) {
+    int pre = 0,n = 0;
+    for (int i = 1; i < loc.size(); i++) {
+        if (loc[i] - pre < mid)
+            n++;
         else
-            // 否则将第一个比 e 大的元素修改为 e（降低了子序列中 e 位置的大小，使序列增长的概率增大）
-            *it = e;
+            pre = loc[i];
     }
-    cout << lis.size();
+    return n <= m;
 }
 
 int main() {
-    lis();
+    int tl, n, m;
+    cin >> tl >> n >> m;
+    if (n == m) {
+        cout << tl;
+        return 0;
+    }
+    vector<int> loc(n + 2);
+
+    for (int i = 1; i <= n; i++)
+        cin >> loc[i];
+
+    loc[n + 1] = tl;
+    // https://www.luogu.com.cn/blog/HOJQVFNA/qian-tan-er-fen-di-bian-jie-wen-ti
+    int l = 1, r = tl, mid;
+    // 不记录 mid 时 使用 l < r
+    while (l < r) {
+        mid = (l + r + 1) >> 1;
+        if (check(loc, mid, m))
+            // + 1 上移至 /2 处
+            l = mid;
+        else
+            r = mid - 1;
+    }
+    cout << l;
     return 0;
 }
