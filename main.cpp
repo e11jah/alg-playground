b2ced2600694e66ede8d1aef88789f3f4dd74612
F1renze <f1renze.142857@gmail.com>
2021-12-19 14:42:21 +0800

    lg: p1478 摘苹果 贪心

--- main.cpp
+++ main.cpp
@@ -9,46 +9,27 @@ using namespace std;
 using PII = pair<int, int>;
 typedef long long ll;
 
-int ans = INT32_MAX, arr[6][11], memo[26][6], path[6], minPath[6];
-
-void dfs(int km, int p, int t) {
-    if (km < 0)
-        return;
-    // 记忆化
-    if (memo[km][p] < t)
-        return;
-    if (p > 5) {
-        if (km == 0 && t < ans) {
-            // 更新最优 dfs 路径
-            for (int i = 1; i < 6; i++)
-                minPath[i] = path[i];
-            ans = t < ans ? t : ans;
-        }
-        return;
-    }
-    // 剩 km 公里时 p 个人参加的最小开销
-    memo[km][p] = t;
-
-    for (int i = 1; i <= 10; i++) {
-        // 当前选手跑了 i 公里
-        path[p] = i;
-        dfs(km - i, p + 1, t + arr[p][i]);
-    }
-}
 
 int main() {
-    for (int i = 0; i < 26; i++)
-        for (int j = 0; j < 6; j++)
-            memo[i][j] = INT32_MAX;
+    int n, s, a, b, h, ans = 0, idx = 0;
+    cin >> n >> s >> a >> b;
+    h = a + b;
+    vector<PII> apples;
+    apples.reserve(n);
+    PII p;
+    for (int i = 0; i < n; i++) {
+        cin >> p.first >> p.second;
+        if (p.first <= h)
+            apples.emplace_back(p.first, p.second);
+    }
+    sort(apples.begin(), apples.end(), [](PII l, PII r) -> bool {
+        return l.second < r.second;
+    });
+    while (s >= 0 && idx < apples.size() && s >= apples[idx].second) {
+        s -= apples[idx++].second;
+        ans ++;
+    }
+    cout << ans;
 
-    for (int i = 1; i <= 5; i++)
-        for (int j = 1; j <= 10; j++)
-            cin >> arr[i][j];
-
-    dfs(25, 1, 0);
-    cout << ans << endl;
-
-    for (int i = 1; i <= 5; i++)
-        cout << minPath[i] << " ";
     return 0;
 }
