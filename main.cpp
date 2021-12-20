49411f919631a76c98502dfc95f4a2952f65eb8d
F1renze <f1renze.142857@gmail.com>
2021-12-20 23:44:05 +0800

    lg: p1280 尼克任务 线性 dp

--- main.cpp
+++ main.cpp
@@ -9,26 +9,57 @@ using namespace std;
 using PII = pair<int, int>;
 typedef long long ll;
 
-int main() {
-    int h, l1 = 0, l2 = 0;
-    vector<int> v, idx, remains;
-    while (cin >> h) {
-        v.push_back(h);
-    }
-    int n = v.size();
-    vector<int> lds(n), lis(n);
-    lds[0] = lis[0] = v[0];
-    for (int i = 1; i < n; i++) {
-        if (lds[l1] >= v[i])
-            lds[++l1] = v[i];
-        else
-            // 默认只能对上升序列使用，对下降序列使用需更换为 greater
-            *upper_bound(lds.begin(), lds.begin()+l1, v[i], greater()) = v[i];
+void v1() {
+    int n , k;
+    cin >> n >> k;
+    vector<PII> v(k);
+    vector<int> sum(n+1), dp(n+1);
 
-        if (lis[l2] < v[i])
-            lis[++l2] = v[i];
-        else
-            *lower_bound(lis.begin(), lis.begin()+l2, v[i]) = v[i];
+    for (int i = 0; i < k; i++) {
+        cin >> v[i].first >> v[i].second;
+        sum[v[i].first]++;
     }
-    cout << l1+1 << endl << l2+1;
+    sort(v.begin(), v.end(), [](PII l, PII r) {
+        return l.first < r.first;
+    });
+    int cur = k-1;
+    // 按总分钟数从后往前遍历
+    for (int i = n; i > 0; i--) {
+        if (sum[i]<1)
+            // 无任务时取上一分钟的空闲时间 + 1
+            dp[i] = dp[i+1] + 1;
+        else for (int j = 0; j < sum[i]; j++) {
+                // 有任务时比较当前空闲时间与放弃该任务时的空闲时间
+                dp[i] = dp[i] > dp[i+v[cur].second] ? dp[i] : dp[i+v[cur].second];
+                cur--;
+            }
+    }
+    cout << dp[1];
+}
+
+/**
+ * 二维数组免排序法
+ */
+void v2() {
+    int n, k, a, b;
+    cin >> n >> k;
+    vector<vector<int>> v(n+1);
+    vector<int> dp(n+1);
+    for (int i = 0; i < k; i++) {
+        cin >> a >> b;
+        v[a].push_back(b);
+    }
+
+    for (int i = n; i > 0; i--) {
+        if (v[i].empty())
+            dp[i] = dp[i+1] + 1;
+        else for (int j = 0; j < v[i].size(); j++)
+            dp[i] = dp[i] > dp[i+v[i][j]] ? dp[i] : dp[i+v[i][j]];
+
+    }
+    cout << dp[1];
+}
+
+int main() {
+    v2();
 }
