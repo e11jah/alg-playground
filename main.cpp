e38da8955fbb338c3e7eee6de88f004ef609dd4f
F1renze <f1renze.142857@gmail.com>
2021-12-23 18:40:22 +0800

    lg: p1095 守望者逃离 （思维题）

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <vector>
 #include <queue>
+#include <math.h>
 
 using namespace std;
 
@@ -38,79 +39,26 @@ void unSolve() {
     cout << ans;
 }
 
-PII getTime(const string &t) {
-    size_t f = t.find_first_of(':');
-    string sh = t.substr(0, f), sm = t.substr(f + 1);
-    int h = 0, m = 0;
-    for (char c : sh)
-        h = h * 10 + c - '0';
-    for (char c : sm)
-        m = m * 10 + c - '0';
-    return {h, m};
-}
-
-int n, totalMin;
-vector<int> t, c, p, dp;
-
-// 多重背包板子
-void v1() {
-
-
-    int maxTimes;
-    // dp 下标含义 第 i 个
-    for (int i = 0; i < n; i++) {
-        // j 剩余时间（背包容量）
-        for (int j = totalMin; j >= 0; j--) {
-            // 看无限次时取总时间能看的最大次数
-            maxTimes = p[i] < 1 ? totalMin / t[i] : p[i];
-            // 枚举每种看的次数方案 (容量 j >= 次数 * 价值)
-            for (int k = 1; k <= maxTimes && j >= k * t[i]; k++) {
-                // 加上的价值要乘上次数
-                dp[j] = max(dp[j], dp[j - k * t[i]] + k * c[i]);
-            }
-        }
-    }
-    cout << dp[totalMin];
-}
-
-// 多重背包 二进制优化
-void v2() {
-    for (int i = 0; i < n; i++) {
-        // 完全背包
-        if (p[i] < 1 || t[i] * p[i] > totalMin) {
-            for (int j = t[i]; j <= totalMin; j++)
-                dp[j] = max(dp[j], dp[j - t[i]] + c[i]);
-        } else {
-            // 分堆, 每次对比 减去容量的，故分堆后与遍历 p[i] 次效果一样
-            for (int k = 1; k <= p[i]; k <<= 1) {
-                // 01 背包
-                for (int j = totalMin; j >= k * t[i]; j--)
-                    dp[j] = max(dp[j], dp[j - k * t[i]] + k * c[i]);
-                p[i] -= k;
-            }
-            // 剩余的数量再次进行 01 背包
-            if (p[i])
-                for (int j = totalMin; j >= p[i] * t[i]; j--)
-                    dp[j] = max(dp[j], dp[j - p[i] * t[i]] + p[i] * c[i]);
-        }
-    }
-    cout << dp[totalMin];
-}
-
 int main() {
-    string st, end;
-    cin >> st >> end;
-    PII now = getTime(st), run = getTime(end);
+    // 向上取整做法，无余数 -1 结果加回
+//    cout << (2 / 4-1)+1 << endl;
+    int m, s, t;
+    cin >> m >> s >> t;
+    int s1 = 0, s2 = 0;
+    for (int i = 1; i <= t; i++) {
+        s2 += 17;
+        if (m >= 10)
+            s1 += 60, m -= 10;
+        else
+            m += 4;
+        if (s1 > s2)
+            s2 = s1;
 
-    if (run.first < now.first)
-        run.first += 24;
-    totalMin = run.second + 60 * (run.first - now.first) - now.second;
+        if (s1 >= s || s2 >= s) {
+            cout << "Yes" << endl << i;
+            return 0;
+        }
+    }
 
-    cin >> n;
-    t = vector<int>(n), c = vector<int>(n), p = vector<int>(n), dp = vector<int>(totalMin + 1);
-
-    for (int i = 0; i < n; i++)
-        cin >> t[i] >> c[i] >> p[i];
-
-    v2();
+    cout << "No" << endl << max(s1, s2);
 }
