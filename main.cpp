46269d3e8a059b21b39b2c4c47addf401f5da946
F1renze <f1renze.142857@gmail.com>
2021-12-23 15:55:26 +0800

    lg: p1833 樱花 （多重背包板子 + 二进制优化）

--- main.cpp
+++ main.cpp
@@ -38,29 +38,79 @@ void unSolve() {
     cout << ans;
 }
 
-int main() {
-    int n, m, MOD = 1e6 + 7;
-    // 第 i 种，余 k 个位置, 摆 j 个
-//    int dp[101][101][101];
-    cin >> n >> m;
+PII getTime(const string &t) {
+    size_t f = t.find_first_of(':');
+    string sh = t.substr(0, f), sm = t.substr(f + 1);
+    int h = 0, m = 0;
+    for (char c : sh)
+        h = h * 10 + c - '0';
+    for (char c : sm)
+        m = m * 10 + c - '0';
+    return {h, m};
+}
 
-    // 多重背包
-    int dp[101];
-    memset(dp, 0, sizeof  dp);
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    // 不摆时有且仅有 1 种方案
-    dp[0] = 1;
+int n, totalMin;
+vector<int> t, c, p, dp;
+
+// 多重背包板子
+void v1() {
+
+
+    int maxTimes;
+    // dp 下标含义 第 i 个
     for (int i = 0; i < n; i++) {
-        for (int k = m; k>=0; k--) {
-            // 满足 j 不大于 ai && 余下的位置能够放 j 个
-            for (int j = 1; j <= min(a[i], k); j++) {
-                //背包 dp[i][j] = max(dp[i-1][j], dp[i-1][k-j] + v[i])
-                // k-j = 0 时即 k 自己
-                dp[k] = (dp[k] + dp[k-j]) % MOD;
+        // j 剩余时间（背包容量）
+        for (int j = totalMin; j >= 0; j--) {
+            // 看无限次时取总时间能看的最大次数
+            maxTimes = p[i] < 1 ? totalMin / t[i] : p[i];
+            // 枚举每种看的次数方案 (容量 j >= 次数 * 价值)
+            for (int k = 1; k <= maxTimes && j >= k * t[i]; k++) {
+                // 加上的价值要乘上次数
+                dp[j] = max(dp[j], dp[j - k * t[i]] + k * c[i]);
             }
         }
     }
-    cout << dp[m];
+    cout << dp[totalMin];
+}
+
+// 多重背包 二进制优化
+void v2() {
+    for (int i = 0; i < n; i++) {
+        // 完全背包
+        if (p[i] < 1 || t[i] * p[i] > totalMin) {
+            for (int j = t[i]; j <= totalMin; j++)
+                dp[j] = max(dp[j], dp[j - t[i]] + c[i]);
+        } else {
+            // 分堆, 每次对比 减去容量的，故分堆后与遍历 p[i] 次效果一样
+            for (int k = 1; k <= p[i]; k <<= 1) {
+                // 01 背包
+                for (int j = totalMin; j >= k * t[i]; j--)
+                    dp[j] = max(dp[j], dp[j - k * t[i]] + k * c[i]);
+                p[i] -= k;
+            }
+            // 剩余的数量再次进行 01 背包
+            if (p[i])
+                for (int j = totalMin; j >= p[i] * t[i]; j--)
+                    dp[j] = max(dp[j], dp[j - p[i] * t[i]] + p[i] * c[i]);
+        }
+    }
+    cout << dp[totalMin];
+}
+
+int main() {
+    string st, end;
+    cin >> st >> end;
+    PII now = getTime(st), run = getTime(end);
+
+    if (run.first < now.first)
+        run.first += 24;
+    totalMin = run.second + 60 * (run.first - now.first) - now.second;
+
+    cin >> n;
+    t = vector<int>(n), c = vector<int>(n), p = vector<int>(n), dp = vector<int>(totalMin + 1);
+
+    for (int i = 0; i < n; i++)
+        cin >> t[i] >> c[i] >> p[i];
+
+    v2();
 }
