1edff1c1d7c593ade67c081e00e17f30bef25b4d
F1renze <f1renze.142857@gmail.com>
2021-12-25 00:42:53 +0800

    lg: P1064 今明预算方案 01背包变种

--- main.cpp
+++ main.cpp
@@ -10,44 +10,40 @@ using namespace std;
 using PII = pair<int, int>;
 typedef long long ll;
 
-
-const int N = 500;
-int mx[N][N], vis[N][N], ans = INT_MAX, dx[4] = {0, 0, -1, 1}, dy[4] = {1, -1, 0, 0};
-
-void dfs(int x, int y, int t) {
-    if (x < 0 || y < 0 || t >= mx[x][y] || t >= ans || t >= vis[x][y])
-        return;
-
-
-    vis[x][y] = t;
-    if (mx[x][y] == INT_MAX) {
-        ans = ans > t ? t : ans;
-        return;
-    }
-    for (int i = 0; i < 4; i++)
-        dfs(x + dx[i], y + dy[i], t + 1);
-}
-
 int main() {
 
     ios::sync_with_stdio(false);
-    fill(&mx[0][0], &mx[N - 1][N - 1], INT_MAX);
-    fill(&vis[0][0], &vis[N - 1][N - 1], INT_MAX);
+    int n, m, v, p, q;
+    cin >> n >> m;
+    vector<PII> pp(m + 1);
+    vector<vector<PII>> aa(m + 1);
+    ll dp[n + 1];
+    memset(dp, 0, sizeof dp);
 
-    int m;
-    cin >> m;
-    int x, y, t;
     for (int i = 0; i < m; i++) {
-        cin >> x >> y >> t;
-        mx[x][y] = min(mx[x][y], t);
-
-        for (int j = 0; j < 4; j++)
-            if (x + dx[j] >= 0 && y + dy[j] >= 0)
-                mx[x + dx[j]][y+dy[j]] = min(mx[x+dx[j]][y+dy[j]], t);
+        cin >> v >> p >> q;
+        if (!q) {
+            // 先计算价值 v 与 开销 w
+            pp[i + 1].first = v * p, pp[i + 1].second = v;
+        } else {
+            aa[q].emplace_back(v * p, v);
+        }
     }
-    dfs(0, 0, 0);
-    if (ans == INT_MAX)
-        cout << -1;
-    else
-        cout << ans;
+    PII t, a1, a2;
+    for (int i = 1; i <= m; i++) {
+        t = pp[i];
+        for (int j = n; t.second > 0 && j >= t.second; j -= 10) {
+            dp[j] = max(dp[j], dp[j - t.second] + t.first);
+            if (aa[i].empty())
+                continue;
+            a1 = aa[i][0], a2 = aa[i][1];
+            if (j >= t.second + a1.second)
+                dp[j] = max(dp[j], dp[j - t.second - a1.second] + t.first + a1.first);
+            if (j >= t.second + a2.second)
+                dp[j] = max(dp[j], dp[j - t.second - a2.second] + t.first + a2.first);
+            if (j >= t.second + a1.second + a2.second)
+                dp[j] = max(dp[j], dp[j - t.second - a1.second-a2.second] + t.first + a1.first + a2.first) ;
+        }
+    }
+    cout << dp[n];
 }
