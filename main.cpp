df9c7436e31d88a1db9904acff6c8da375a7d356
F1renze <f1renze.142857@gmail.com>
2021-12-28 20:41:47 +0800

    lg: P1005 矩阵取数 记忆化搜索 + 区间 dp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,48 @@
-//#include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
 using namespace std;
-
-using PII = pair<int, int>;
-#define bll __int128
-
-typedef long long ll;
-
-const int N = 120;
-int m;
-bll pow2[N], memo[N][N], v[N];
-
-template<typename T>
-inline T
-read() {  // 声明 template 类,要求提供输入的类型T,并以此类型定义内联函数 read()
-    T sum = 0, fl = 1;  // 将 sum,fl 和 ch 以输入的类型定义
-    int ch = getchar();
-    for (; !isdigit(ch); ch = getchar())
-        if (ch == '-') fl = -1;
-    for (; isdigit(ch); ch = getchar()) sum = sum * 10 + ch - '0';
-    return sum * fl;
-}
-
-inline void write(__int128 x) {
-    static __int128 sta[35];
-    int top = 0;
-    do {
-        sta[top++] = x % 10, x /= 10;
-    } while (x);
-    while (top) putchar(sta[--top] + 48);  // 48 是 '0'
-}
-
-
-#define min3(a, b, c) min(a, min(b, c))
-#define max3(a, b, c) max(a, max(b, c))
-
-bll dfs(int l, int r) {
-    if (memo[l][r] > -1)
-        return memo[l][r];
-    if (r-l<1)
-        return memo[l][r] = v[l] * pow2[m-r+l];
-    return memo[l][r] = max(dfs(l + 1, r) + pow2[m - r + l] * v[l], dfs(l, r - 1) + pow2[m - r + l] * v[r]);
-}
-
-int main() {
-//    ios::sync_with_stdio(false);
-//    std::cin.tie(nullptr);
-    int n;
-    bll ans = 0;
-    cin >> n >> m;
-
-    pow2[0] = 1;
-    for (int i = 1; i <= m; i++)
-        pow2[i] = 2 * pow2[i - 1];
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++)
-            v[j] = read<bll>();
-
-        memset(memo, -1, sizeof(memo));
-        ans += dfs(1, m);
+const int Maxn=1e6+5;
+char s[Maxn];
+int n,m,x[Maxn],y[Maxn],c[Maxn],sa[Maxn];
+int height[Maxn],rk[Maxn];
+void SA()
+{
+    for(int i=1;i<=n;i++){x[i]=s[i];++c[x[i]];}
+    for(int i=2;i<=m;i++)c[i]+=c[i-1];
+    for(int i=n;i>=1;i--)sa[c[x[i]]--]=i;
+    for(int k=1;k<=n;k=k<<1)
+    {
+        int num=0;
+        for (int i=n-k+1;i<=n;++i) y[++num]=i;
+        for(int i=1;i<=n;i++)
+            if(sa[i]>k)y[++num]=sa[i]-k;
+        for(int i=1;i<=m;i++)c[i]=0;
+        for(int i=1;i<=n;i++)c[x[i]]++;
+        for(int i=2;i<=m;i++)c[i]+=c[i-1];
+        for(int i=n;i>=1;i--){sa[c[x[y[i]]]--]=y[i];y[i]=0;}
+        swap(x,y);
+        num=1;x[sa[1]]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(y[sa[i]]==y[sa[i-1]]&&y[sa[i]+k]==y[sa[i-1]+k])x[sa[i]]=num;
+            else x[sa[i]]=++num;
+        }
+        if(num==n)break;
+        m=num;
     }
-
-    write(ans);
+    for(int i=1;i<=n;i++)
+        printf("%d ",sa[i]);
+    printf("\n");
+}
+int main()
+{
+    scanf("%s",s+1);
+    n=strlen(s+1);
+    m=122;
+    SA();
+    SA();
+    //LCP(); 
+    return 0;
 }
